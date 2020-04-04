e0f796ad91ffdbfc8198e6ac0a49414c6059a522
goodmeow <harunbam3@gmail.com>
2020-04-04 16:20:27 +0700

    jasmine_sprout: overlay: update for overlay
    - for fixing multi user, incrase statusbar notification icons, and maintainer name

---
+++ dot_whyred.mk
@@ -0,0 +1,59 @@
+#
+# Copyright (C) 2018-2019 The LineageOS Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_o_mr1.mk)
+
+# Inherit some common Lineage stuff
+$(call inherit-product, vendor/dot/config/common.mk)
+
+# DotOS Properties
+EXTRA_FOD_ANIMATIONS := true
+TARGET_INCLUDE_WIFI_EXT := true
+TARGET_BOOT_ANIMATION_RES := 1080
+TARGET_INCLUDE_LIVE_WALLPAPERS := false
+TARGET_INCLUDE_STOCK_ARCORE := true
+DOT_BUILD_TYPE := REMBANG-BANGKIT
+TARGET_FACE_UNLOCK_SUPPORTED := true
+TARGET_INCLUDE_PIXEL_CHARGER := true
+TARGET_GAPPS_ARCH := arm64
+
+# Lawnchair Launcher
+#$(call inherit-product-if-exists, vendor/lawnchair/lawnchair.mk)
+
+# Inherit from custom vendor
+$(call inherit-product, vendor/MiuiCamera/config.mk)
+
+# Inherit from whyred device
+$(call inherit-product, $(LOCAL_PATH)/device.mk)
+
+PRODUCT_BRAND := Xiaomi
+PRODUCT_DEVICE := whyred
+PRODUCT_MANUFACTURER := Xiaomi
+PRODUCT_NAME := dot_whyred
+PRODUCT_MODEL := Redmi Note 5
+
+PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
+
+TARGET_VENDOR_PRODUCT_NAME := whyred
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+   PRIVATE_BUILD_DESC="whyred-user 8.1.0 OPM1.171019.011 V9.5.11.0.OEIMIFA release-keys"
+
+BUILD_FINGERPRINT := xiaomi/whyred/whyred:8.1.0/OPM1.171019.011/V9.5.11.0.OEIMIFA:user/release-keys
+
