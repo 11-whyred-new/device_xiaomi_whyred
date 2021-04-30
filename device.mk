fdb33d115402b0cb75614119a9766fa7a1ea440e
predator112 <mi560334@gmail.com>
2021-04-30 15:25:21 +0000

    remove

--- device.mk
+++ device.mk
@@ -68,8 +68,7 @@ PRODUCT_PACKAGES += \
 
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
-    $(DEVICE_PATH)/overlay \
-    $(DEVICE_PATH)/overlay-lineage
+    $(DEVICE_PATH)/overlay
 
 # Recorder
 PRODUCT_PACKAGES += \
