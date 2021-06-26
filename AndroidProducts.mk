97a4f34307b8d426219702172f0d3e666c763618
root <root@raphael.ehp0n55wvzhu3m2fz1tduxiosh.lx.internal.cloudapp.net>
2021-06-26 06:27:04 +0000

    xtendes bringup

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -14,9 +14,9 @@
 # limitations under the License.
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/dot_whyred.mk
+    $(LOCAL_DIR)/xtended_whyred.mk
 
 COMMON_LUNCH_CHOICES := \
-    dot_whyred-user \
-    dot_whyred-userdebug \
-    dot_whyred-eng
+    xtended_whyred-user \
+    xtended_whyred-userdebug \
+    xtended_whyred-eng
