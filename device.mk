34cc5fc4b14580fe841f59538e6400c214b0fb63
blankaf <realblankaf@gmail.com>
2021-08-17 03:24:28 +0000

    whyred: rootdir: Adapt for libperfmgr
    Co-authored-by: Ramii Ahmed <ramy@ahmedramy.com>
    Change-Id: Ia5f59d183056728dc2966b99ff74dbb4fcc83c7b

--- device.mk
+++ device.mk
@@ -429,6 +429,7 @@ PRODUCT_PACKAGES += \
     init.qcom.sensors.sh \
     init.qcom.sh \
     init.qcom.usb.rc \
+    init.qcom.power.rc \
     init.qcom.usb.sh \
     init.target.rc \
     ueventd.qcom.rc \
