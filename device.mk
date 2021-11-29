4ca056259ee9c08ca26dd5b482f377ef608e9539
iqbal <whyred@ip-172-31-44-109.us-east-2.compute.internal>
2021-11-29 08:01:31 +0000

    delet powerhal pixel

--- device.mk
+++ device.mk
@@ -384,12 +384,8 @@ PRODUCT_PACKAGES += \
 
 # Power
 PRODUCT_PACKAGES += \
-    android.hardware.power@1.3-service.xiaomi_sdm660-libperfmgr \
+    android.hardware.power@1.2-service-qti \
     vendor.qti.hardware.perf@2.0.vendor
-
-# Powerhint
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/powerhint.json:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.json
 
 # Protobuf
 PRODUCT_PACKAGES += \
